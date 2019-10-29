814ed9cbea4f1dd1d764840b3ee51192f8317c91
Rob Siegwart <rob@robsiegwart.com>
2019-10-29 13:31:17 -0400

    Add config.ini support, click integration

--- README.md
+++ README.md
@@ -2,65 +2,54 @@
 
 A Python program to construct a resume based on data stored in YAML files.
 
-The motivation is to separate data from presentation in order to more quickly generate different forms of a resume based on the desired data to be included. Additionally, different templates can be used in order to create differently formatted versions of a resume from the same set of data.
+The motivation is to easily build different resumes based on modular data. This allows several forms of a resume to be generated at once (e.g. PDF, text, HTML), specifially when content needs to be tailored for a specific aim. Additionally, different templates can be used in order to create differently formatted versions of a resume from the same content (e.g. regular resume, CV-type resume).
+
+Tailoring of a resume can be accomplished by creating a new folder with only those YAML files having the changed content.
 
 ## Usage
 
 ### File Structure
 
-       resume.py                           (program)
-       [Resume.py]                         (run file)
-    +- Publish                             (output directory)
-         +- [publish directories]
-         +- ...
-    +- Resume Data                         (resume data files)
-         +- Default
-         +- ...                            (additional data directories)
+       resume.py
+       config.ini                   <- configuration file
+       
+    +- Publish/                     <- output directory
+       +- [publish directories]
+       +- ...
+
+    +- Resume Data                  <- resume data files
+       +- Default/
+       +- ...
+
+    +- Templates/                   <- templates directory
+       +- html/
+          default.html              <- Default HTML template
+          css/
+             style.css
+             bootstrap.min.css
+             ...
+       +- text/
+          default.txt               <- Default text template
+
+### Command Line Tool
+```
+Usage: resume.py build [OPTIONS] SOURCE_DIR
+
+  Generate html, pdf, and text versions of a resume and save them in a
+  directory.
+
+Options:
+  --html_template TEXT  Template to use for HTML rendering.
+  --text_template TEXT  Template name to use for text rendering.
+  --title               Toggle title on/off.
+  --help                Show this message and exit.
+```
 
 ### Resume Data
 
 Data is stored in YAML files within a directory in the `Resume Data` directory. A `Default` directory must exist, which serves as the base set of data. Additional directories are then added which overide the data of Default. Only data that changes needs included in these directories.
 
 Templates currently use the following files and properties:
-
-#### Certifications.yaml
-
-    Certifications :
-      <Cert> :
-        <property>: <value>
-        <property>: <value>
-
-#### Education.yaml
-
-    Education:
-      degree : <degree>
-      school : <name>
-      date: <date>
-      address: <address>
-
-#### Experience.yaml
-
-    Experience :
-      <company 1 name> :
-        location: <location>
-        positions :
-          <position name> :
-            dates : <dates>
-          <position name> :
-            dates : <dates>
-        summary : >
-          <summary paragraph>
-        selected achievements :
-          - <item>
-          - <item>
-          - ...
-      <company 2 name> :
-        location : <location>
-        positions :
-          <position name> :
-            dates : <dates>
-        summary : >
-          <summary paragraph>
 
 #### Header.yaml
 
@@ -72,61 +61,116 @@ Templates currently use the following files and properties:
         email   : <value>
         web     : <value>
 
-#### Patents.yaml
-
-    Patents :
-      <Patent name> :
-        patent number : <number>
-        issued : <date>
-        description : <value>
-
 #### Performance profile.yaml
 
-    Performance profile : >
-        <paragraph>
+    <string>
 
 #### Skills.yaml
 
-    Skills :
-      <category header> :
-        - <skill>
-        - <skill>
-        - <skill>
-      <category header> :
-        - <skill>
-        - <skill>
+    <category title> :
+      - <skill>
+      - <skill>
+      - <skill>
+    
+    (repeat)
+
+#### Experience.yaml
+
+    <company 1 name> :
+      location: <location>
+      positions :
+        <position 1 name> :
+          dates : <dates>
+        <position 2 name> :
+          dates : <dates>
+      summary : >
+        <summary paragraph>
+      selected achievements :
+        - <item>
+        - <item>
+        - ...
+    
+    (repeat)
+
+#### Education.yaml
+
+    <degree> :
+      school : <name>
+      date: <date>
+      address: <address>
+      gpa: <number>
+    
+    (repeat)
 
 #### Training.yaml
 
-    Training :
-      <title>:
-        description : <description>
-      <title>:
-        description : <description>
+    <title>:
+      description : <description>
+    
+    (repeat)
 
-## Installation
+#### Certifications.yaml
 
-### 3rd Party Applications
+    <Certification name> :
+      <property>: <value>
+      ...
 
-`wkhtmltopdf` (wkhtmltopdf.org)
+    (repeat)
 
-### Python Packages
+#### Patents.yaml
+
+    <Patent name> :
+      patent number : <number>
+      issued : <date>
+      description : <value>
+
+
+## config.ini
+
+A `config.ini` file may be placed in the top-level directory with the following as valid settings (the pdf options are just those for `pdfkit`):
+
+  - PDF_MARGIN_TOP
+  - PDF_MARGIN_BOTTOM
+  - PDF_MARGIN_LEFT
+  - PDF_MARGIN_RIGHT
+    * Set the margins in the pdf file; accepts a string with units (e.g. '0.75in')
+  - PDF_PAGE_SIZE
+    * 'Letter', 'A4', etc.
+  - PDF_DISABLE_EXTERNAL_LINKS
+    * Include this and set to 'False' to enable this flag
+  - SKILLS_LAYOUT
+    * Enter a pipe and comma separated string denoting the columns and order of headers in the "Skills" section. For example:
+      `engineering, programming | writing, data analysis | software, fabrication`
+    This tells it to create 3 columns with the order of the headings as listed. The headings must match those listed in the `Skills.yaml` file.
+
+## Dependencies
+
+**3rd Party Applications:**
+
+ - wkhtmltopdf (wkhtmltopdf.org)
+
+**Python Packages:**
 
 - Jinja2
 - pdfkit
 - PyYAML
+- click
 
-## Example
+## Example Usage
 
 ### Resume Data
 
-### Build File
+With the following data inIn folder *Default*:
 
-    from resume import build
-    
-    options = {
-        template = '',
-        source_dir = ''
-    }
-    
-    build(name[, options])      # options default to 'default'
+**Header.yaml**
+
+
+### Build
+
+```
+$ python resume.py build Default
+```
+
+### Output
+
+
--- resume.py
+++ resume.py
@@ -1,70 +1,87 @@
 '''
-    Create a HTML version of a resume based on YAML data files.
-
+    Title:          Resume Builder Module
+    Description:    Create a HTML version of a resume based on YAML data files
+                    and convert to PDF.
 '''
 
 import os
 from datetime import datetime
 from glob import glob
 from collections import OrderedDict
+import configparser
 import yaml, pdfkit
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import click
 
 
+@click.group()
+def cli():
+    pass
 
-TEMPLATES_DIR = 'Templates'
-SOURCES_DIR = 'Resume Data'
-PUBLISH_DIR = 'Publish'
+
+def load_config():
+    Config = configparser.ConfigParser(defaults={ 'TEMPLATES_DIR': 'Templates',
+                                                  'SOURCES_DIR': 'Resume Data',
+                                                  'PUBLISH_DIR': 'Publish',
+                                                  'PDF_DISABLE_EXTERNAL_LINKS': False })
+
+    Config.read('config.ini')
+    
+    # set any properties that should be parsed with other than just get()
+    dtypes = { 'PDF_DISABLE_EXTERNAL_LINKS': Config.getboolean }
+
+    CONFIG = {}
+
+    for key in Config['DEFAULT']:
+        keyu = key.upper()
+        if keyu in dtypes.keys():
+            CONFIG[keyu] = dtypes[keyu]('DEFAULT', key)
+        else:
+            CONFIG[keyu] = Config.get('DEFAULT', key)
+
+    CONFIG['PDF_OPTIONS'] = { 'margin-top': CONFIG['PDF_MARGIN_TOP'],
+                              'margin-right': CONFIG['PDF_MARGIN_RIGHT'],
+                              'margin-bottom': CONFIG['PDF_MARGIN_BOTTOM'],
+                              'margin-left': CONFIG['PDF_MARGIN_LEFT'],
+                              'disable-external-links': CONFIG['PDF_DISABLE_EXTERNAL_LINKS'] }
+    
+    return CONFIG
 
 
-def build_resume(output_file, source_dir='default', html_template='default',
-                 text_template='default', skills_col=4, skills_layout=None,
-                 title=True, pdf_options=None):
-    ''' Main build command. Generates html, pdf, and text versions of resume and
-        saves them in a directory.
-
-    Parameters
-    ----------
-    output_file : str
-        base name of output file(s) (w/o extension)
-    source_dir : str
-        name of directory to load data from
-    html_template : str
-        name of template to use for html/pdf generation
-    text_template : str
-        name of template to use for text generation
-    skills_col : int
-        number of columns for the skills section
-    skills_layout : list
-        list of lists containing the skills headings in order to be printed
-    title : boolean
-        show or not the title field
-    pdf_options : dict
-        a dictionary of parameters to send to wkhtmltopdf
-        
+@click.command()
+@click.argument('source_dir')
+@click.option('--html_template', default='default', help="Template to use for HTML rendering.")
+@click.option('--text_template', default='default', help="Template name to use for text rendering.")
+@click.option('--title', default=False, is_flag=True, help="Toggle title on/off.")
+def build(source_dir, html_template, text_template, title):
     '''
-    context = {'title': title}
+        Generate html, pdf, and text versions of a resume and save them in a
+        directory.
+    '''
+    context = { 'title': title }
+
+    CONFIG = load_config()
 
     # Create a new directory to put files in (autorename to avoid overwriting)
-    new_folder = datetime.today().strftime('%d%b-%Y') + ' - ' + output_file
-    if os.path.exists(os.path.join(PUBLISH_DIR,new_folder)):
+    new_folder = source_dir + ' - ' + datetime.today().strftime('%d%b-%Y')
+    if os.path.exists(os.path.join(CONFIG['PUBLISH_DIR'],new_folder)):
         i = 1
-        while os.path.exists(os.path.join(PUBLISH_DIR,new_folder+' ({})'.format(i))):
+        while os.path.exists(os.path.join(CONFIG['PUBLISH_DIR'],new_folder+' ({})'.format(i))):
             i += 1
         new_folder = new_folder+' ({})'.format(i)
-    os.mkdir(os.path.join(PUBLISH_DIR,new_folder))
+    os.mkdir(os.path.join(CONFIG['PUBLISH_DIR'],new_folder))
     
-    out_dir = os.path.join(PUBLISH_DIR,new_folder)
-    output_file = os.path.join(out_dir,output_file)
-    context['TEMPLATE_DIR_REL'] = os.path.relpath(TEMPLATES_DIR,out_dir)
+    out_dir = os.path.join(CONFIG['PUBLISH_DIR'],new_folder)
+    output_file = os.path.join(out_dir,source_dir)
+    context['TEMPLATE_DIR_REL'] = os.path.relpath(CONFIG['TEMPLATES_DIR'],out_dir)
 
     # Load resume data into variable 'context'
-    #  - only changed files need to be added to new directory
+    #  - only changed files are added to new directory
     #  - all other unchanged data will be loaded from 'default' directory
 
-    source_files = glob(SOURCES_DIR+'\\{}\\*.yaml'.format(source_dir))
+    source_files = glob(CONFIG['SOURCES_DIR']+'\\{}\\*.yaml'.format(source_dir))
     source_file_names = list(map(lambda f: os.path.basename(f), source_files))
-    default_files = glob(SOURCES_DIR+'\\Default\\*.yaml')
+    default_files = glob(CONFIG['SOURCES_DIR']+'\\Default\\*.yaml')
 
     for fname in default_files:
         if os.path.basename(fname) in source_file_names:
@@ -72,12 +89,12 @@ def build_resume(output_file, source_dir='default', html_template='default',
             fname = list(filter(lambda f: bn in f, source_files))[0]
         with open(fname) as f:
             name = os.path.basename(fname)
-            name = os.path.splitext(name)[0].lower().replace('-','_').replace(' ','_')
+            name = os.path.splitext(name)[0].replace('-','_').replace(' ','_')
             context[name] = yaml.safe_load(f)
-
+    
     # Setup Jinja templating
     env = Environment(
-        loader=FileSystemLoader([os.path.join(TEMPLATES_DIR,'html'),os.path.join(TEMPLATES_DIR,'text')]),
+        loader=FileSystemLoader([os.path.join(CONFIG['TEMPLATES_DIR'],'html'),os.path.join(CONFIG['TEMPLATES_DIR'],'text')]),
         autoescape=select_autoescape(['html']),
         trim_blocks=True
     )
@@ -92,39 +109,29 @@ def build_resume(output_file, source_dir='default', html_template='default',
     html_template = load_template('html', html_template)
     text_template = load_template('txt', text_template)
     
-    # Layout the skills section if it exceeds the number of columns
-    cats = len(context.get('skills').get('Skills').keys())       # number of skill categories
-    if cats > skills_col:
-        if not skills_layout:
-            skills_layout = []                                   # category labels for each column (ordered left to right)
-            skills = OrderedDict(sorted(context['skills']['Skills'].items(), key = lambda t: len(t[1]), reverse=True))  # sort by num of skills
-            b = 1
-            for i,cat in enumerate(skills.keys()):
-                if i < skills_col:
-                    skills_layout.append([cat])
-                else:
-                    skills_layout[skills_col-b].append(cat)
-                    b += 1
-        context['skills_layout'] = skills_layout
+    # Layout the skills section if configured to do so
+    if CONFIG.get('SKILLS_LAYOUT'):
+        sl = list(map(lambda x: x.split(','), CONFIG['SKILLS_LAYOUT'].split('|')))
+        for i,each in enumerate(sl):
+            sl[i] = list(map(lambda x: x.strip(), each))
 
-    # Create and save the html version
+        context['skills_layout'] = sl
+
+    print(context['skills_layout'])
+
+    # HTML
     html = html_template.render(context=context)
     save_file(html,output_file+'.html')
-    
-    # Convert and save the pdf file
+
+    # PDF
     pdf_in = output_file+'.html'
     pdf_out = output_file+'.pdf'
-    print('Creating pdf ...')
-    if pdf_options:
-        pdf_options = { **pdf_options_default, **pdf_options}
-    else:
-        pdf_options = pdf_options_default
+    pdfkit.from_file(pdf_in, pdf_out, options=CONFIG['PDF_OPTIONS'])
+    print('\nSaved file "{}"'.format(pdf_out))
 
-    pdfkit.from_file(pdf_in, pdf_out, options=pdf_options)
-
-    # Create and save the text version
-    # text = text_template.render(context=context)
-    # save_file(text,output_file+'.txt')
+    # TEXT
+    text = text_template.render(context=context)
+    save_file(text,output_file+'.txt')
 
 
 def save_file(content,output_file):
@@ -134,11 +141,8 @@ def save_file(content,output_file):
     print('\nSaved file "{}"'.format(output_file))
 
 
-pdf_options_default = {
-    'margin-top': '14',       # mm
-    'margin-bottom': '5',     # mm
-    'margin-left': '14',      # mm
-    'margin-right': '14',     # mm
-    'page-size': 'Letter',
-    'disable-external-links': None,
-}
+cli.add_command(build)
+
+
+if __name__ == '__main__':
+    cli()
